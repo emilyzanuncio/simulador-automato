676e17d9f018278d62a40b6b9af1e2fca3938c56
Emily <emilyzanun@gmail.com>
2024-11-11 14:16:49 -0400

    Adicionar opção para importar arquivos .txt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import filedialog
+from model import inserirFA, modelosFA, mostrarFA
+
+global impWindow
 
 def novaJanela(controller):
     janela = tk.Tk()
@@ -8,23 +11,42 @@ def novaJanela(controller):
     tk.Label(janela, text="SimulAutômato", font=('Arial',18)).grid(row=0,column=0,padx=20,pady=20)
     tk.Label(janela, text="Selecione uma das opções:", font=('Arial',12)).grid(row=1,column=0)
     
-    tk.Button(janela, text="Importar Autômato", command=importarFA).grid(row=2,column=0,padx=20,pady=10)
-    tk.Button(janela, text="Modelos Prontos", command=importarFA).grid(row=3,column=0,padx=20,pady=10)
-    tk.Button(janela, text="Escrever Autômato", command=importarFA).grid(row=4,column=0,padx=20,pady=10)
+    tk.Button(janela, text="Importar Autômato", command=importar).grid(row=2,column=0,padx=20,pady=10)
+    tk.Button(janela, text="Modelos Prontos", command=modelos).grid(row=3,column=0,padx=20,pady=10)
+    tk.Button(janela, text="Escrever Autômato", command=inserir).grid(row=4,column=0,padx=20,pady=10)
     
     janela.mainloop()
 
-def importarFA():
-    print("Importar FA")
+def importar():
+    global impWindow
+    impWindow = tk.Tk()
+    impWindow.title("SimulAutômato")
     
-def inserirFA():
-    print("Inserir FA")
+    tk.Label(impWindow, text="IMPORTAR .TXT", font=('Arial',18)).grid(row=0,column=0,padx=20,pady=20)
+    tk.Label(impWindow, text="Importe um autômato:").grid(row=1,column=0,padx=10,pady=5)
     
-def modelosFA():
-    print("Modelos Exemplo")
+    tk.Button(impWindow, text="Selecionar arquivo", command=importarTxt).grid(row=2,column=0,padx=20,pady=5)
+    
+    impWindow.mainloop()
 
-def mostrarFA():
-    print("Mostrar FA")
+def importarTxt():
+    global impWindow
+    print("TXT")
+    file_path = filedialog.askopenfilename(
+        filetypes=[("Text Files", "*.txt")])
+    
+    if file_path:
+        tk.Label(impWindow,text="Arquivo localizado!").grid(row=3,column=0)
+        tk.Button(impWindow,text="Simular", command=mostrarFA).grid(row=4,column=0,pady=10)
+
+def modelos():
+    print("Modelos")
+    modelosFA()
+
+def inserir():
+    print("Escrever")
+    inserirFA()
+
 
 def main():
     janela = novaJanela(controller=globals())
