050fc7ee6504888b0ce84f239716d1d3221d53ea
Emily <emilyzanun@gmail.com>
2024-11-11 16:46:19 -0400

    Adicionar função para verificar seções e conteúdos do arquivo .TXT

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import visual_automata
+import re
 
 def simularFA(automato):
     f = open(automato,"r")
@@ -16,3 +16,42 @@ def salvarSimular(caixaTexto):
         file.write(entrada)
         
     mostrarFA(automato)
+    
+def confereTXT(automato):
+    secObrigatorias = ["#states","#initial","#accepting","#alphabet","#transitions"]
+    secoes = {}
+    
+    modeloGeral = re.compile(r"\w")
+    transicaoModelo = re.compile(r"\w+:\w+>\w+(,\w+)*$")
+    
+    # Ler os conteúdos do arquivo e organizar as seções
+    with open(automato,"r") as file:
+        secAtual = None
+        for linha in file:
+            linha = linha.strip()
+            if linha in secObrigatorias: # Se a linha lida estiver na lista secObg.
+                secAtual = linha
+                secoes[secAtual] = [] # Inserir seção atual nas seções contidas
+            elif secAtual:
+                secoes[secAtual].append(linha)
+    
+    naoLocalizadas = [sec for sec in secObrigatorias if sec not in secoes]
+    if naoLocalizadas:
+        print(f"ERRO: Seções obrigatórias faltando.")
+        return False
+    
+    for estado in secoes["#states"]:
+        if not modeloGeral.match(estado):
+            print("ERRO: Formato errado de estado.")
+    for estado in secoes["#initial"]:
+        if not modeloGeral.match(estado):
+            print("ERRO: Formato errado de estado inicial.")
+    for estado in secoes["#accepting"]:
+        if not modeloGeral.match(estado):
+            print("ERRO: Formato errado de estado final.")
+    for estado in secoes["#alphabet"]:
+        if not modeloGeral.match(estado):
+            print("ERRO: Formato errado de alfabeto.")
+    for transicao in secoes["#transitions"]:
+        if not transicaoModelo.match(transicao):
+            print("ERRO: Formato errado de transição.")
