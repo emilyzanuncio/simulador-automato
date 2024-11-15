d0d8b659d2af58f873652a187e7ea88c04a9097a
Emily <emilyzanun@gmail.com>
2024-11-15 18:08:37 -0400

    Adicionar função busca de cadeia em um autômato

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, scrolledtext
-from model import validaAutomato, simularFA, salvarSimular
+from model import validaAutomato, salvarSimular
 
 global mainWindow, impWindow, mdWindow, addWindow
 
@@ -20,8 +20,8 @@ def novaJanela(controller):
 
 def importar():
     global impWindow
-    impWindow = tk.Tk()
-    impWindow.title("SimulAutômato")
+    impWindow = tk.Toplevel(mainWindow)
+    impWindow.title("SimulAutômato - Importar Autômato")
     
     tk.Label(impWindow, text="IMPORTAR .TXT", font=('Arial',18)).grid(row=0,column=0,padx=20,pady=20)
     tk.Label(impWindow, text="Importe um autômato:").grid(row=1,column=0,padx=10,pady=5)
@@ -41,33 +41,34 @@ def importarTxt():
 
 def modelos():
     global mdWindow
-    mdWindow = tk.Tk()
-    mdWindow.title("SimulAutômato")
+    mdWindow = tk.Toplevel()
+    mdWindow.title("SimulAutômato - Modelos Disponíveis")
     
     tk.Label(mdWindow, text="MODELOS", font=('Arial',18)).grid(row=0,column=0,padx=20,pady=20)
     tk.Label(mdWindow, text="Autômatos disponíveis", font=('Arial',14)).grid(row=1,column=0,padx=10,pady=5)
     
     realFA = "./modelosFA/reconheceReal.txt"
     thompsonAFD = "./modelosFA/thompsonAFD.txt"
+    thompsonAFNe = "./modelosFA/thompsonAFNe.txt"
     erro = "./modelosFA/erro.txt"
     
     tk.Button(mdWindow, text="Real", command=lambda: validaAutomato(realFA)).grid(row=2,column=0,padx=20,pady=5)
     tk.Button(mdWindow, text="(a+b)*a - AFD", command=lambda: validaAutomato(thompsonAFD)).grid(row=3,column=0,padx=20,pady=5)
-    tk.Button(mdWindow, text="exemplo de erro", command=lambda: validaAutomato(erro)).grid(row=4,column=0,padx=20,pady=5)
-    #tk.Button(impWindow, text="(a+b)*a - AFN", command=validaAutomato).grid(row=4,column=0,padx=20,pady=5)
+    tk.Button(mdWindow, text="(a+b)*a - AFN-e", command=lambda: validaAutomato(thompsonAFNe)).grid(row=4,column=0,padx=20,pady=5)
+    tk.Button(mdWindow, text="exemplo de erro", command=lambda: validaAutomato(erro)).grid(row=5,column=0,padx=20,pady=5)
     
     mdWindow.mainloop()
 
 def inserir():
     global addWindow
     addWindow = tk.Tk()
-    addWindow.title("SimulAutômato")
+    addWindow.title("SimulAutômato - Inserir Autômato")
     # Título da Janela de Inserção
     tk.Label(addWindow, text="Escreva seu autômato", font=('Arial',14)).grid(row=0,column=0,padx=20,pady=20)
     
     tk.Label(addWindow,text="Utilize o formato:\n#states\ns0\ns1\n#initial\ns0\n#accepting\ns1\n#alphabet\na\nb\n#transitions\ns0:a>s0\ns0:b>s0,s1",
              font=('Arial',12)).grid(row=1,column=0,pady=5)
-    tk.Label(addWindow, text="Não deixe linhas vazias.", font=('Arial',12)).grid(row=2,column=0,padx=20,pady=20)
+    tk.Label(addWindow, text="Delete linhas vazias.\nUse $ para transições vazias.", font=('Arial',12)).grid(row=2,column=0,padx=20,pady=20)
     
     caixaTexto = scrolledtext.ScrolledText(addWindow, wrap=tk.WORD, width=40, height=8)
     caixaTexto.grid(row=3,column=0,padx=10,pady=10)
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ def validaAutomato(automato):
     valido,mensagem = confereTXT(automato)
     # Caso seja válido, iniciar simulação. Se não, enviar mensagem de erro.
     if valido:
-        simularFA(automato)
+        simuladorJanela()
     else:
         messagebox.showerror("ERRO", mensagem)
 
@@ -22,9 +22,10 @@ def salvarSimular(caixaTexto):
     
 def confereTXT(automato):
     secObrigatorias = ["#states","#initial","#accepting","#alphabet","#transitions"]
+    global secoes 
     secoes = {}
     # Define o modelo das transições estadoA:simbolo>estadoB
-    transicaoModelo = re.compile(r"\w+:(\w+|\$)>\w+(,\w+)*$")
+    transicaoModelo = re.compile(r"\w+:(\w+|\$|\,)>\w+(,\w+)*$")
     
     # Ler os conteúdos do arquivo e organizar as seções
     with open(automato,"r") as file:
@@ -77,19 +78,22 @@ def confereTXT(automato):
     for transicao in secoes["#transitions"]:
         # Verifica se as transições listadas seguem o modelo estadoA:simbolo>estadoB
         if not transicaoModelo.match(transicao):
-            erro = "Formato errado de transição."
+            erro = f"Formato errado de transição."
             return False, erro
         # Verifica se as transições listadas usam os estados e alfabeto definidos
         estadoOrigem,resto = transicao.split(":") # Copia estadoA para estadoOrigem e o resto da regEx para resto
         simbolo,estadoDestino = resto.split(">") # Copia estadoB para estadoDestino
         # Verifica se o estado origem da transição está nos estados definidos
         if estadoOrigem not in estadosDefinidos:
-            erro = f"ERRO: O estado {estadoOrigem} não é parte do autômato definido."
+            erro = f"O estado {estadoOrigem} não é parte do autômato definido."
             return False, erro
         # Verifica se o estado destino da transição está nos estados definidos
-        if estadoDestino not in estadosDefinidos:
-            erro = f"O estado {estadoDestino} não é parte do autômato definido."
-            return False, erro
+        estadoDestino = estadoDestino.split(",")
+        for destino in estadoDestino:
+            if destino not in estadosDefinidos:
+                erro = f"O estado {destino} não é parte do autômato definido."
+                return False, erro
+        
         # Verifica se o símbolo da transição está no alfabeto definido (permite transição vazia)
         if simbolo not in alfabetoDefinido and simbolo != '$':
             erro = f"O símbolo '{simbolo}' não é parte do alfabeto definido."
@@ -97,12 +101,83 @@ def confereTXT(automato):
     sucesso = "Autômato aceito."
     return True, sucesso
 
-def simularFA(automato):
-    #f = open(automato,"r")
-    #print(f.read())
-    #================================
-    print("Simulando!")
-    #================================
+def simuladorJanela():
+    janelaSimulador = tk.Tk()
+    janelaSimulador.title("SimulAutômato - Simulador")
+    # Título da janela do simulador
+    tk.Label(janelaSimulador,text="SimulAutômato", font=('Arial',14)).pack(padx=20,pady=10)
+    
+    tk.Label(janelaSimulador,text="Insira a palavra a ser buscada:",font=('Arial',12)).pack(padx=25,pady=5)
+    palavra = tk.Entry(janelaSimulador,width=20)
+    palavra.pack(pady=(0,20))
+    tk.Button(janelaSimulador,text="Buscar",command=lambda: buscaGUI(palavra)).pack(pady=10)
+    
+    janelaSimulador.mainloop()
 
 def mostrarFA(automato):
     print("Mostrando visualmente")
+    
+def buscaGUI(palavra):
+    palavra = palavra.get()
+    # Inicia a simulação e envia os resultados para as variáveis encontrada e caminhoFinal
+    encontrada, caminhoFinal = simularAutomato(palavra)
+    
+    print("Caminho percorrido:",caminhoFinal)
+    # Abre uma caixa de mensagens para mostrar o resultado
+    if encontrada:
+        messagebox.showinfo("Resultado","A cadeia pertence ao autômato.\nCheque o terminal para o caminho percorrido.")
+    else:
+        messagebox.showerror("Resultado","A cadeia não pertence ao autômato.\nCheque o terminal para o caminho percorrido.")    
+
+def simularAutomato(palavra):
+    #estados = set(secoes["#states"]) # Copia estados p/ variável estados
+    inicial = secoes["#initial"][0] # Copia estado inicial p/ variável inicial
+    finais = set(secoes["#accepting"]) # Copia estados finais p/ variável finais
+    transicoes = secoes["#transitions"] # Copia transições p/ variável transicoes
+    #alfabeto = set(secoes["#alphabet"]) # Copia alfabeto p/ variável alfabeto
+    #palavra = palavra.get()
+    caminhoFinal = []
+    encontrada = False
+    #fimSimulacao = False
+    
+    def buscar(estadoAtual, posicao, caminhoAtual):
+        nonlocal caminhoFinal, encontrada
+        # Atualiza o caminho
+        caminhoAtual = caminhoAtual + [estadoAtual]
+        # Atualiza caminho final
+        caminhoFinal = caminhoAtual
+        # Caso a palavra tenha sido lida por completo
+        if posicao == len(palavra):
+            if estadoAtual in finais:
+                encontrada = True
+                return encontrada,caminhoFinal
+            else:
+                return encontrada,caminhoFinal
+        
+        # Não continuar caso a cadeia tenha sido encontrada
+        if encontrada:
+            return True
+        
+        # Processar transições normais (símbolo ∈ alfabeto)
+        simbolo = palavra[posicao] # simbolo recebe o símbolo na posição atual
+        
+        for transicao in transicoes:
+            origem, resto = transicao.split(":") # Separa o estado origem do resto
+            simboloTransicao, destinos = resto.split(">") # Separa o símbolo dos estados destino
+            destinos = destinos.split(",") # Separa os estados destino
+            # Caso o estadoAtual corresponda ao inicial e o simbolo atual ao da transição, percorrer avançar
+            if origem == estadoAtual and simboloTransicao == simbolo:
+                for destino in destinos:
+                    transicaoExecutada = f"{simboloTransicao}>{destino}"
+                    buscar(destino, posicao + 1, caminhoAtual + [transicaoExecutada])
+            # Caso o estado atual possua transições em vazio
+            elif origem == estadoAtual and simboloTransicao == "$":
+                for destino in destinos:
+                    transicaoExecutada = f"{simboloTransicao}>{destino}"
+                    buscar(destino, posicao, caminhoAtual + [transicaoExecutada])
+        
+    # Iniciar busca
+    buscar(inicial, 0, [])
+    # Enviar resultados
+    caminhoFinal = caminhoFinal
+    return encontrada, caminhoFinal
